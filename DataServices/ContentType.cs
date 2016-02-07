900054bf4347f59614caebc51f9f981bc6c596b2
marcinszydlak <marcinszydlak93@wp.pl>
2016-02-07 16:50:44 +0100

    Obsluzon zarzadzenie plikami

---
+++ DataServices/ContentType.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServices
+{
+    public class StringValue : System.Attribute
+    {
+        private string _value;
+
+        public StringValue(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+
+    }
+    public enum ContentType
+    {
+        [StringValue("application/pdf")]
+        PDF = 1,
+        [StringValue("audio/mpeg")]
+        MP3 = 2,
+        [StringValue("audio/x-ms-wma")]
+        WMA = 3,
+        [StringValue("audio/x-wav")]
+        WAV = 4,
+        [StringValue("image/gif")]
+        GIF = 5,
+        [StringValue("image/png")]
+        PNG = 6,
+        [StringValue("image/jpeg")]
+        JPG = 7,
+        [StringValue("application/msword")]
+        DOC = 8,
+        [StringValue("application/vnd.openxmlformats-officedocument.wordprocessingml.document")]
+        DOCX = 9,
+        [StringValue("application/vnd.ms-excel")]
+        XLS = 10,
+        [StringValue("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")]
+        XLSX = 11,
+        [StringValue("application/vnd.ms-powerpoint")]
+        PPT = 12,
+        [StringValue("application/vnd.openxmlformats-officedocument.presentationml.presentation")]
+        PPTX = 13,
+        [StringValue("text/plain")]
+        TXT = 14,
+        [StringValue("application/x-rar-compressed")]
+        RAR = 15,
+        [StringValue("application/zip")]
+        ZIP = 16,
+
+    }
+}
---
+++ DataServices/FileServices.cs
@@ -0,0 +1,58 @@
+﻿using Database;
+using DataServices.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Reflection;
+
+namespace DataServices
+{
+    public class FileServices
+    {
+        public string GetContentType(string Extension)
+        {
+            switch(Extension.ToUpper())
+            {
+                case "PDF": return GetStringValue(ContentType.PDF);
+                case "MP3": return GetStringValue(ContentType.MP3);
+                case "WMA": return GetStringValue(ContentType.WMA);
+                case "WAV": return GetStringValue(ContentType.WAV);
+                case "GIF": return GetStringValue(ContentType.GIF);
+                case "PNG": return GetStringValue(ContentType.PNG);
+                case "JPG": return GetStringValue(ContentType.JPG);
+                case "DOC": return GetStringValue(ContentType.DOC);
+                case "DOCX": return GetStringValue(ContentType.DOCX);
+                case "XLS": return GetStringValue(ContentType.XLS);
+                case "XLSX": return GetStringValue(ContentType.XLSX);
+                case "PPT": return GetStringValue(ContentType.PPT);
+                case "PPTX": return GetStringValue(ContentType.PPTX);
+                case "TXT": return GetStringValue(ContentType.TXT); 
+                case "C": return GetStringValue(ContentType.TXT);
+                case "CPP": return GetStringValue(ContentType.TXT);
+                case "CS": return GetStringValue(ContentType.TXT);
+                case "JAVA": return GetStringValue(ContentType.TXT);
+                case "RAR": return GetStringValue(ContentType.RAR);
+                case "ZIP" :return GetStringValue(ContentType.ZIP);
+                default : throw new Exception("Aplikacja nie obsługuje rozszerzenia : "+ Extension);
+            }
+        }
+        public static string GetStringValue(Enum value)
+        {
+            string output = null;
+            Type type = value.GetType();
+            FieldInfo fi = type.GetField(value.ToString());
+            StringValue[] attrs =
+               fi.GetCustomAttributes(typeof(StringValue),
+                                       false) as StringValue[];
+            if (attrs.Length > 0)
+            {
+                output = attrs[0].Value;
+            }
+
+            return output;
+        }
+    }
+}
